c991eba4792c8521431db4983c8de51727f0daac
dgrajewski <dawids115@gmail.com>
2020-06-04 20:06:50 +0200

    Refactor of default objects;

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { initialize } from './data/context/postgres.context';
 require('dotenv').config();
-const { AppModule } = require('./app.module');
+const { AppModule } = require('./module/app.module');
 
 async function bootstrap() {
   await initialize();
---
+++ server/src/module/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from '../controller/app.controller';
+import { JwtTokenStrategy } from '../security/token/passport/jwt.token.strategy';
+import { AppService } from '../service/app.service';
+import { AccountModule } from './account.module';
+import { UserModule } from './user.module';
+
+@Module({
+  imports: [
+    AccountModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    JwtTokenStrategy
+  ],
+})
+export class AppModule { }
