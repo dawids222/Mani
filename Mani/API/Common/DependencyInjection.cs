be9363c144db6c986488cc97935aa44d83c747b3
dgrajewski <dawids115@gmail.com>
2021-02-13 23:15:42 +0100

    Refactor;

---
+++ Mani/API/Common/DependencyInjection.cs
@@ -0,0 +1,69 @@
+﻿using API.Extensions;
+using API.Pipelines;
+using API.Services;
+using Application.Business.Users.GetUsersQuery;
+using Application.Common.Data;
+using Application.Common.Logging;
+using Application.Common.Mapping;
+using Application.Common.Resources.String;
+using Application.Common.Security.Encryption;
+using Application.Common.Security.JWT;
+using Application.Repositories;
+using Application.Requests.Handlers;
+using Common.Logging;
+using Common.Mapping;
+using DAL;
+using DAL.Handlers;
+using DAL.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Resources.String;
+using System;
+
+namespace API.Common
+{
+    internal static class DependencyInjection
+    {
+        internal static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ILogger, LogLiteLogger>();
+
+            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddValidatorsFromAssembly(typeof(GetUsersQuery).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PipelineValidationBehavior<,>));
+            services.AddAuthorizersFromAssembly(typeof(GetUsersQuery).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PipelineAuthorizationBehavior<,>));
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DatabaseConnection")));
+            services.AddTransient<IUsersRepository, UsersRepository>();
+            services.AddTransient<ISettingsRepository, SettingsRepository>();
+            services.AddTransient<IAccountsRepository, AccountsRepository>();
+
+            services.AddTransient<ICurrentUserService, CurrentUserService>();
+
+            services.AddTransient<IEncryptor, PBKDF2Encyptor>();
+            services.AddTransient<IJwtService, JwtService>();
+
+            services.AddTransient<IEntityMapper, MapsterEntityMapper>();
+
+            services.AddTransient(typeof(IOrderByQueryHandler<>), typeof(OrderByQueryHandler<>));
+            services.AddTransient(typeof(ISearchQueryHandler<>), typeof(SearchQueryHandler<>));
+            services.AddTransient(typeof(IFilterQueryHandler<>), typeof(FilterQueryHandler<>));
+            services.AddTransient(typeof(IPaginationQueryHandler<>), typeof(PaginationQueryHandler<>));
+            services.AddTransient(typeof(IAdvancedQueryHandler<>), typeof(AdvancedQueryHandler<>));
+
+            services.AddTransient<IStringResources>(provider =>
+            {
+                var httpContext = provider.GetRequiredService<IHttpContextAccessor>().HttpContext;
+                var locale = httpContext.Request.Headers["Accept-Language"];
+                var stringResourcesFactory = new StringResourcesFactory(locale);
+                return stringResourcesFactory.GetStringResources();
+            });
+        }
+    }
+}
--- Mani/API/Startup.cs
+++ Mani/API/Startup.cs
@@ -1,37 +1,13 @@
 using API.Common;
-using API.Extensions;
 using API.Filters;
-using API.Pipelines;
-using API.Services;
-using Application.Business.Users.GetUsersQuery;
-using Application.Common.Data;
-using Application.Common.Logging;
-using Application.Common.Mapping;
-using Application.Common.Resources.String;
-using Application.Common.Security.Encryption;
-using Application.Common.Security.JWT;
-using Application.Repositories;
-using Application.Requests.Handlers;
-using Common.Logging;
-using Common.Mapping;
-using DAL;
-using DAL.Handlers;
-using DAL.Repositories;
-using FluentValidation;
-using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Resources.String;
-using System;
 using System.Text;
 
 namespace API
@@ -76,40 +52,7 @@ namespace API
                 };
             });
 
-            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSingleton<ILogger, LogLiteLogger>();
-
-            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddValidatorsFromAssembly(typeof(GetUsersQuery).Assembly);
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PipelineValidationBehavior<,>));
-            services.AddAuthorizersFromAssembly(typeof(GetUsersQuery).Assembly);
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PipelineAuthorizationBehavior<,>));
-
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DatabaseConnection")));
-            services.AddTransient<IUsersRepository, UsersRepository>();
-            services.AddTransient<ISettingsRepository, SettingsRepository>();
-            services.AddTransient<IAccountsRepository, AccountsRepository>();
-
-            services.AddTransient<ICurrentUserService, CurrentUserService>();
-
-            services.AddTransient<IEncryptor, PBKDF2Encyptor>();
-            services.AddTransient<IJwtService, JwtService>();
-
-            services.AddTransient<IEntityMapper, MapsterEntityMapper>();
-
-            services.AddTransient(typeof(IOrderByQueryHandler<>), typeof(OrderByQueryHandler<>));
-            services.AddTransient(typeof(ISearchQueryHandler<>), typeof(SearchQueryHandler<>));
-            services.AddTransient(typeof(IFilterQueryHandler<>), typeof(FilterQueryHandler<>));
-            services.AddTransient(typeof(IPaginationQueryHandler<>), typeof(PaginationQueryHandler<>));
-            services.AddTransient(typeof(IAdvancedQueryHandler<>), typeof(AdvancedQueryHandler<>));
-
-            services.AddTransient<IStringResources>(provider =>
-            {
-                var httpContext = provider.GetRequiredService<IHttpContextAccessor>().HttpContext;
-                var locale = httpContext.Request.Headers["Accept-Language"];
-                var stringResourcesFactory = new StringResourcesFactory(locale);
-                return stringResourcesFactory.GetStringResources();
-            });
+            DependencyInjection.Configure(services, Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
