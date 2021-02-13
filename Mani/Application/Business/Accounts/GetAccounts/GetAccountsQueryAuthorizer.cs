f0f1350105cc99526674b0c02ae04651052e2f08
dgrajewski <dawids115@gmail.com>
2021-02-13 23:18:52 +0100

    Added [GET] api/accounts validator and authorizer;

---
+++ Mani/Application/Business/Accounts/GetAccounts/GetAccountsQueryAuthorizer.cs
@@ -0,0 +1,13 @@
+﻿using Application.Authorization.Contract;
+using Application.Authorization.Requirements;
+
+namespace Application.Business.Accounts.GetAccounts
+{
+    public class GetAccountsQueryAuthorizer : AbstractRequestAuthorizer<GetAccountsQuery>
+    {
+        public override void BuildPolicy(GetAccountsQuery instance)
+        {
+            UseRequirement(new MustBeAuthenticatedRequirement());
+        }
+    }
+}
---
+++ Mani/Application/Business/Accounts/GetAccounts/GetAccountsQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Resources.String;
+using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.Business.Accounts.GetAccounts
+{
+    public class GetAccountsQueryValidator : AbstractValidator<GetAccountsQuery>
+    {
+        public GetAccountsQueryValidator(IStringResources resources)
+        {
+            RuleFor(r => r.Query)
+                .SetValidator(new AdvancedQueryValidator(resources));
+        }
+    }
+}
