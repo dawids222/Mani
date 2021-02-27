4a5194181248335e02960a21742c334b973906bb
dgrajewski <dawids115@gmail.com>
2021-02-27 12:14:26 +0100

    Refactor;

--- Mani/Application/Business/Accounts/GetAccounts/GetAccountsQueryVm.cs
+++ Mani/Application/Business/Accounts/GetAccounts/GetAccountsQueryVm.cs
@@ -1,13 +1,22 @@
-﻿using Application.Business.Accounts.GetAccount;
-using Application.Requests.Responses;
+﻿using Application.Requests.Responses;
 using System.Collections.Generic;
 
 namespace Application.Business.Accounts.GetAccounts
 {
-    public class GetAccountsQueryVm : PaginationVm<GetAccountQueryVm>
+    public class GetAccountsQueryVm : PaginationVm<GetAccountsQueryVmListItem>
     {
         public GetAccountsQueryVm(
-            IEnumerable<GetAccountQueryVm> items,
+            IEnumerable<GetAccountsQueryVmListItem> items,
             int allItemsCount) : base(items, allItemsCount) { }
+    }
+
+    public class GetAccountsQueryVmListItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Logo { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
     }
 }
